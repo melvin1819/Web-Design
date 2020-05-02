6f5fb565cdce0012f2acef12c821c04a0f929c19
melvin1819 <62163552+melvin1819@users.noreply.github.com>
2020-05-02 09:18:09 +0800

    AACS 1193 Web-Design and Development

---
+++ Menshoes2.js
@@ -0,0 +1,141 @@
+let carts = document.querySelectorAll('.add-cart');
+
+
+let products = [
+    {
+        name:'Mx-720 Shoes',
+        tag:'mx-720 Shoes',
+        price: 130,
+        inCart: 0 
+    },
+    {
+        name:'Air Max Shoes',
+        tag:'air max shoes',
+        price: 140,
+        inCart: 0 
+    }
+    
+    
+];
+for(let i = 0;i < carts.length;i++){
+    carts[i].addEventListener('click',()=>{
+       cartNumbers(products[i]);
+       totalCost(products[i])
+    })
+
+}
+ 
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+
+function cartNumbers(product) {
+    
+     let productNumbers = localStorage.getItem('cartNumbers');
+
+     productNumbers = parseInt(productNumbers);
+
+     if(productNumbers){
+         localStorage.setItem('cartNumbers',productNumbers + 1);
+         document.querySelector('.cart span').textContent = productNumbers +1;
+     }else{
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+     }
+    
+    setItem(product);
+}
+
+function setItem(product){
+    
+   let cartItems = localStorage.getItem('productsInCart');
+   cartItems = JSON.parse(cartItems);
+    
+   if(cartItems != null){
+       if(cartItems[product.tag] == undefined){
+           cartItems = {
+               ...cartItems,
+               [product.tag]: product
+           }
+       }
+       cartItems[product.tag].inCart += 1;
+   }else{
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+   }
+    localStorage.setItem("productsInCart", JSON.stringify
+    (cartItems));
+}
+function totalCost(product){
+
+    let cartCost = localStorage.getItem('totalCost');
+    console.log("My cartCost is",cartCost);
+    console.log(typeof cartCost);
+
+    if(cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost",cartCost + product.price);
+    }else{
+        localStorage.setItem("totalCost",product.price);
+    }
+}
+
+
+function displayCart(){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector
+    (".products");
+    let cartCost = localStorage.getItem('totalCost');
+    console.log(cartItems);
+    if( cartItems && productContainer){
+        
+      productContainer.innerHTML ='';
+      Object.values(cartItems).map(item =>{
+        productContainer.innerHTML += `
+       
+           <div class="product">
+              <ion-icon name="checkmark-circle"></ion-icon>
+              <img src="./men Shoes/${item.tag}.jpg">
+              <span>${item.name}</span>
+            </div>  
+            <div class="price">$${item.price},00</div>
+            <div class="quantity">
+                <ion-icon name="cart-outline"></ion-icon>
+                <span>${item.inCart}</span>
+                
+            </div>
+            <div class="total">
+                $${item.inCart * item.price},00
+            </div>
+           
+           `;
+      });
+     
+      productContainer.innerHTML += `
+         <div class="basketTotalContainer">
+             <h4 class="basketTotalTitle">
+            Basket Total 
+            </h4>
+           <h4 class="basketTotal">
+                $${cartCost},00 
+                </h4>
+         
+
+        
+                  
+       `;     
+     }
+}
+
+onLoadCartNumbers();
+displayCart();
+
+
